778cfbcff0b789bc62c07922116231ef55eb14f2
YuriyEG <you@example.com>
2024-08-26 18:20:06 +1200

    fix: ~

--- cities/src/components/Processing.tsx
+++ cities/src/components/Processing.tsx
@@ -1,17 +1,29 @@
 import { FC } from "preact/compat";
+
 import styled from "styled-components";
+
 import { Spin } from "antd";
 import ErrorMessage from "./ErrorMessage";
+
 import { observer } from "mobx-react-lite";
 import { useStores } from "../root-store-context";
 
 const Container = styled.div`
   width: 100%;
-  height: 50px;
+  height: auto;
   margin-bottom: 20px;
   transition: max-height 0.4s ease-in-out;
   overflow: hidden;
   max-height: ${({ isOpen }) => (isOpen ? "200px" : "0")};
+`;
+
+const LoadingContainer = styled.div`
+  width: 100%;
+  padding-left: 10px;
+`;
+
+const LoadingText = styled.p`
+  margin: 0;
 `;
 
 const Processing: FC = observer(() => {
@@ -24,12 +36,10 @@ const Processing: FC = observer(() => {
   return (
     <Container isOpen={hasContent}>
       {isLoading && (
-        <div
-          style={{ width: "100%", paddingLeft: "10px", marginBottom: "20px" }}
-        >
+        <LoadingContainer>
           <Spin style={{ marginBottom: "12px" }} />
-          <p>Идет загрузка... </p>
-        </div>
+          <LoadingText>Идет загрузка...</LoadingText>
+        </LoadingContainer>
       )}
       {isError && <ErrorMessage message={errorMessage} />}
       {invalidToken && <ErrorMessage message="Не валидный токен!" />}
