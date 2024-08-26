4126d5fb82d00a0dcb600e332dcec29546fb4d9d
YuriyEG <you@example.com>
2024-08-26 18:05:48 +1200

    feat: effect added to Processing.tsx

--- cities/src/components/Processing.tsx
+++ cities/src/components/Processing.tsx
@@ -1,11 +1,7 @@
 import { FC } from "preact/compat";
-
 import styled from "styled-components";
-
 import { Spin } from "antd";
-
 import ErrorMessage from "./ErrorMessage";
-
 import { observer } from "mobx-react-lite";
 import { useStores } from "../root-store-context";
 
@@ -13,6 +9,9 @@ const Container = styled.div`
   width: 100%;
   height: auto;
   margin-bottom: 20px;
+  transition: max-height 0.3s ease-in-out;
+  overflow: hidden;
+  max-height: ${({ isOpen }) => (isOpen ? "200px" : "0")};
 `;
 
 const Processing: FC = observer(() => {
@@ -20,14 +19,16 @@ const Processing: FC = observer(() => {
     cities: { isError, isLoading, errorMessage, invalidToken },
   } = useStores();
 
+  const hasContent = isLoading || isError || invalidToken;
+
   return (
-    <Container>
+    <Container isOpen={hasContent}>
       {isLoading && (
         <div
           style={{ width: "100%", paddingLeft: "10px", marginBottom: "20px" }}
         >
           <Spin style={{ marginBottom: "12px" }} />
-          <p>Идет загузка... </p>
+          <p>Идет загрузка... </p>
         </div>
       )}
       {isError && <ErrorMessage message={errorMessage} />}
